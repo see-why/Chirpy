6a5dff46813835532766931598ecc579c442d418
Iyadi Cyril Osita <iyadicy@gmail.com>
2025-01-17 23:01:40 -0500

    http file server

---
+++ main.go
@@ -0,0 +1,64 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"sync/atomic"
+	"time"
+)
+
+type apiConfig struct {
+	fileserverHits atomic.Int32
+}
+
+func (cfg *apiConfig) middlewareMetricInc(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		cfg.fileserverHits.Add(1)
+		fmt.Printf("Hits: %d\n", cfg.fileserverHits.Load())
+		next.ServeHTTP(w, r)
+	})
+}
+
+func (cfg *apiConfig) middlewareGetMetrics(w http.ResponseWriter, req *http.Request) {
+	w.WriteHeader(http.StatusOK)
+	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+	w.Write([]byte(fmt.Sprintf("Hits: %d", cfg.fileserverHits.Load())))
+}
+
+func (cfg *apiConfig) middlewareResetMetrics(w http.ResponseWriter, req *http.Request) {
+	w.WriteHeader(http.StatusOK)
+	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+	cfg.fileserverHits.Store(0)
+	w.Write([]byte(fmt.Sprintf("Hits: %d", cfg.fileserverHits.Load())))
+}
+
+var apiCfg = &apiConfig{}
+
+func main() {
+	const filepathRoot = "."
+	const addr = ":8080"
+
+	m := http.NewServeMux()
+	m.Handle("/app/", http.StripPrefix("/app", apiCfg.middlewareMetricInc(http.FileServer(http.Dir(filepathRoot)))))
+	m.HandleFunc("/healthz", func(w http.ResponseWriter, req *http.Request) {
+		w.WriteHeader(http.StatusOK)
+		w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+		w.Write([]byte(http.StatusText(http.StatusOK)))
+	})
+	m.HandleFunc("/metrics", apiCfg.middlewareGetMetrics)
+	m.HandleFunc("/reset", apiCfg.middlewareResetMetrics)
+
+	srv := http.Server{
+		Handler:      m,
+		Addr:         addr,
+		WriteTimeout: 30 * time.Second,
+		ReadTimeout:  30 * time.Second,
+	}
+
+	// this blocks forever, until the server
+	// has an unrecoverable error
+	fmt.Println("server started on ", addr)
+	err := srv.ListenAndServe()
+	log.Fatal(err)
+}
