da9f9f4eaf9a2aa9aa1651ee1a6cf09109a38bc4
Iyadi Cyril Osita <iyadicy@gmail.com>
2025-01-23 01:58:19 -0500

    sqlc code for fetching all chirps

--- internal/database/chirps.sql.go
+++ internal/database/chirps.sql.go
@@ -40,3 +40,36 @@ func (q *Queries) CreateChirp(ctx context.Context, arg CreateChirpParams) (Chirp
 	)
 	return i, err
 }
+
+const selectChirps = `-- name: SelectChirps :many
+SELECT id, user_id, body, created_at, updated_at FROM chirps
+`
+
+func (q *Queries) SelectChirps(ctx context.Context) ([]Chirp, error) {
+	rows, err := q.db.QueryContext(ctx, selectChirps)
+	if err != nil {
+		return nil, err
+	}
+	defer rows.Close()
+	var items []Chirp
+	for rows.Next() {
+		var i Chirp
+		if err := rows.Scan(
+			&i.ID,
+			&i.UserID,
+			&i.Body,
+			&i.CreatedAt,
+			&i.UpdatedAt,
+		); err != nil {
+			return nil, err
+		}
+		items = append(items, i)
+	}
+	if err := rows.Close(); err != nil {
+		return nil, err
+	}
+	if err := rows.Err(); err != nil {
+		return nil, err
+	}
+	return items, nil
+}
