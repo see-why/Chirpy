21049c230eaa638f9e939bbb7c58134cb16cfe24
Iyadi Cyril Osita <iyadicy@gmail.com>
2025-01-24 22:42:43 -0500

    authenticate and hash password

---
+++ internal/auth.go
@@ -0,0 +1,22 @@
+package auth
+
+import (
+	"errors"
+
+	"golang.org/x/crypto/bcrypt"
+)
+
+func HashPassword(password string) (string, error) {
+	if password == "" {
+		return "", errors.New("password cannot be empty")
+	}
+	hash, err := bcrypt.GenerateFromPassword([]byte(password), bcrypt.DefaultCost)
+	if err != nil {
+		return "", err
+	}
+	return string(hash), nil
+}
+
+func ComparePassword(hashedPassword, password string) error {
+	return bcrypt.CompareHashAndPassword([]byte(hashedPassword), []byte(password))
+}
